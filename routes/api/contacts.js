bb3c9d4cc2e60923661354e39df158d993fe0532
Roman <rpetrenko0105@gmail.com>
2023-11-01 16:17:07 +0200

    edit route
    edit route for
     router.patch("/:id/favorite", CtrlWraper(ctrl.updateFavorite))

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,7 +8,7 @@ router.get("/", CtrlWraper(ctrl.getList));
 router.get("/:id", CtrlWraper(ctrl.getContactById));
 router.post("/", CtrlWraper(ctrl.addContact));
 router.put("/:id", CtrlWraper(ctrl.updateContact));
-router.patch("/:id", CtrlWraper(ctrl.updateFavorite));
+router.patch("/:id/favorite", CtrlWraper(ctrl.updateFavorite));
 router.delete("/:id", CtrlWraper(ctrl.removeContact));
 
 module.exports = router;
